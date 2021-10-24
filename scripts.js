1ea5c8eeb0bb97b39c243daaf02ddfc657f9a2aa
KayviaHarriott <kayviaharriott@hotmail.com>
2021-10-24 12:36:05 -0500

    Finished email verification

--- scripts.js
+++ scripts.js
@@ -1,22 +1,32 @@
 /* Add your JavaScript to this file */
-
 document.addEventListener('DOMContentLoaded', function() {
-    var form = document.querySelector('form');
-    form.onsubmit = checkForm;
-
+    var form = document.querySelector('form');    
+    var email = document.querySelector('#email');
+    let messageSec = document.getElementsByClassName("message");
     
-    function checkForm() {
-        event.preventDefault();
-        var email = document.querySelector('#email');
-        let messageSec = document.getElementsByClassName("message");
+    form.onsubmit = myFunction();
 
-        if (email.value==' ' || email.value  =='' || email.value == null){
-            messageSec[0].textContent = "Please enter a valid email address";
-            //alert("Hello world!");
-        }
-        else{
-            messageSec[0].textContent = "Thank you! Your email address " + email.value + " has been added to our mailing list!";
-            
-        }
+    function myFunction(){
     }
+
+    form.addEventListener('submit', function(evt){
+        evt.preventDefault();
+
+        if (email.value == "" || email.value == null) {
+            messageSec[0].textContent = "Please enter a valid email address";
+            
+            
+        }        
+        else {
+            email.setCustomValidity('');
+            if ( (email.value.includes("@")==true) && (email.value.includes(".")==true )) {
+                messageSec[0].textContent = "Thank you! Your email address " + email.value + " has been added to our mailing list!";
+            }
+            else{
+                email.setCustomValidity('');
+                messageSec[0].textContent = "Please enter a valid email address";
+            }
+        }
+
+    })
 });
