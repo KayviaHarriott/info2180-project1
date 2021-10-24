bb8b26287d6320d360705f628bbbfda05ab5bdaa
KayviaHarriott <kayviaharriott@hotmail.com>
2021-10-24 09:38:00 -0500

    Modified js file to select form submit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
 /* Add your JavaScript to this file */
 
 document.addEventListener('DOMContentLoaded', function() {
-    alert("Hello world!");
+    var form = document.querySelector('form');
+    var email = document.querySelector('#email');
+    email.focus();
 
+    form.onsubmit = checkForm;
+
+    function checkForm() {
+        //alert(email.value);
+        alert("Hello world!");
+        event.preventDefault();
+    }
 });
