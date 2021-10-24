f29b01588a452665eb706168645b896d0ecff586
KayviaHarriott <kayviaharriott@hotmail.com>
2021-10-24 10:41:37 -0500

    Added email validation

--- scripts.js
+++ scripts.js
@@ -2,14 +2,21 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     var form = document.querySelector('form');
-    var email = document.querySelector('#email');
-    email.focus();
-
     form.onsubmit = checkForm;
 
+    
     function checkForm() {
-        //alert(email.value);
-        alert("Hello world!");
         event.preventDefault();
+        var email = document.querySelector('#email');
+        let messageSec = document.getElementsByClassName("message");
+
+        if (email.value==' ' || email.value  =='' || email.value == null){
+            messageSec[0].textContent = "Please enter a valid email address";
+            //alert("Hello world!");
+        }
+        else{
+            messageSec[0].textContent = "Thank you! Your email address " + email.value + " has been added to our mailing list!";
+            
+        }
     }
 });
